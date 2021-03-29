ac09f80159d5396bffa7d0268a4e0575402134fd
Jianmu Deng <1453400424@qq.com>
2021-03-29 23:38:17 +0800

    ✨ Feat: 1.enter button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,27 @@
 import styles from './index.less';
 import { Button, Row, Col, Carousel, Card } from 'antd';
 import { Link } from 'umi';
-import bg2 from '../assets/bg2.png';
-import bg3 from '../assets/bg3.png';
-import bg4 from '../assets/bg4.png';
-import bg5 from '../assets/bg5.png';
-import bg6 from '../assets/bg6.png';
-import bg7 from '../assets/bg7.png';
+import React, { useState } from 'react';
 
-export default function IndexPage() {
-  const contentStyle: any = {
-    hight: 'auto',
-    color: '#fff',
-    lineHeight: '160px',
-    textAlign: 'center',
-    background: '#364d79',
-    // windth: '50%'
+const IndexPage: React.FC = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const onClick = () => {
+    setClicked(true);
   };
+
   return (
     <div className={styles.bigBox}>
-      <Card className={styles.card}>
-        aaa
-        {/* <Row>
-        <Col span={12}>
-          <Carousel autoplay autoplaySpeed={1000}>
-            <div >
-              <img src={bg1} style={contentStyle}></img>
-            </div>
-            <div >
-              <img src={bg2} style={contentStyle}></img>
-            </div>
-            <div >
-              <img src={bg3} style={contentStyle}></img>
-            </div>
-          </Carousel>
-        </Col>
-        <Col span={12}>
-          <Button>
-            <Link to='/home'>To Home</Link>
-          </Button>
-        </Col>
-      </Row> */}
-      </Card>
+      <button
+        onClick={onClick}
+        className={`${styles['bubbly-button']} ${
+          clicked ? styles['bubbly-button-animate'] : ''
+        }`}
+      >
+        Click me!
+      </button>
     </div>
   );
-}
+};
+
+export default IndexPage;
